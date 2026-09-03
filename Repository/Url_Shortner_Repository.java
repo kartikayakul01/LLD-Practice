59ca5ce66ab0861842b33db36540df7a9ae849fa
Kartikaya Kulshrestha <kartikaya.kul0908@gmail.com>
2026-09-03 10:34:22 +0530

    Adding repository logic and Async slave updation

---
+++ Repository/Url_Shortner_Repository.java
@@ -0,0 +1,30 @@
+package Repository;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Url_Shortner_Repository{
+    private Map<String,String> URL_Master;
+    private Map<String,String> URL_Slave;
+
+    Url_Shortner_Repository(){
+        URL_Master = new ConcurrentHashMap<>();
+        URL_Slave = new ConcurrentHashMap<>();
+    }
+
+    public Boolean save_url(String ShortURL,String Orignalurl){
+        URL_Master.put(ShortURL,Orignalurl);
+        CompletableFuture.runAsync(()-> {
+            System.out.println("---- Updating slave---");
+            URL_Slave.put(ShortURL,Orignalurl);
+            System.out.println("---- slave updated---");
+
+        });
+    }
+    public String get_url(String shorturl){
+        if(!URL_Slave.containsKey(shorturl)){
+            throw new Exception("This url do not exists");
+        }
+        return URL_Slave.get(shorturl);
+    }
+}
